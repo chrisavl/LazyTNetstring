98db203dde46357367e0a1eb9220c8d776c9a92e
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-08-06 12:33:33 +0200

    memoize and invalidate term attrs

--- lib/lazy_tnetstring/term.rb
+++ lib/lazy_tnetstring/term.rb
@@ -15,31 +15,24 @@ module LazyTNetstring
       value_offset # raises InvalidTNetString if offset can not determined
     end
 
+    def type
+      @type ||= data[value_offset + value_length, 1]
+    end
+
     def length
-      value_offset - offset + value_length + 1
+      @length = value_offset - offset + value_length + 1
     end
 
     def value_offset
-      colon_index = data[offset, 10].index(':')
-      raise InvalidTNetString, "no length found in #{data[offset, 10]}..." unless colon_index
-      offset + colon_index + 1
+      @value_offset ||= calculated_value_offset
     end
 
     def value_length
-      data[offset..(value_offset-2)].to_i
+      @value_length ||= data[offset..(value_offset-2)].to_i
     end
 
     def value
-      case type
-      when Type::STRING     then raw_value
-      when Type::INTEGER    then raw_value.to_i
-      when Type::BOOLEAN    then boolean_from_raw_value
-      when Type::NULL       then nil
-      when Type::LIST       then array_from_raw_value
-      when Type::DICTIONARY then LazyTNetstring::DataAccess.new(data, offset, parent, scope)
-      else
-        raise InvalidTNetString, "unknown term type #{type}"
-      end
+      @value ||= typecast_raw_value
     end
 
     def raw_value
@@ -53,10 +46,8 @@ module LazyTNetstring
     def value_length=(new_length)
       new_length_str = new_length.to_s
       data[offset..(value_offset-2)] = new_length_str
-    end
-
-    def type
-      data[value_offset + value_length, 1]
+      @length = nil
+      @value_offset = nil
     end
 
     def to_s
@@ -65,9 +56,32 @@ module LazyTNetstring
 
     private
 
+    def calculated_value_offset
+      colon_index = data[offset, 10].index(':')
+      raise InvalidTNetString, "no length found in #{data[offset, 10]}..." unless colon_index
+      offset + colon_index + 1
+    end
+
     def raw_data=(new_raw_data)
       @data[offset, length] = new_raw_data
       @raw_value = nil
+      @type = nil
+      @length = nil
+      @value_offset = nil
+      @value_length = nil
+    end
+
+    def typecast_raw_value
+      case type
+      when Type::STRING     then raw_value
+      when Type::INTEGER    then raw_value.to_i
+      when Type::BOOLEAN    then boolean_from_raw_value
+      when Type::NULL       then nil
+      when Type::LIST       then array_from_raw_value
+      when Type::DICTIONARY then LazyTNetstring::DataAccess.new(data, offset, parent, scope)
+      else
+        raise InvalidTNetString, "unknown term type #{type}"
+      end
     end
 
     def boolean_from_raw_value
