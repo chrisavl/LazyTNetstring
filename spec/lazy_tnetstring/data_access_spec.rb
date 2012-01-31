chrisavl/LazyTNetstring

commit 835da19a28402adb710dc7bf1fc97e519ed391bb

Fix bug when searching nested dicts out of scope
When searching for a non-existing key in a nested
dictionary the search would previously continue
outside of the nested scope.

lib/lazy_tnetstring/term.rb
@@ -59,7 +59,12 @@ module LazyTNetstring
     def calculated_value_offset
       colon_index = data[offset, 10].index(':')
       raise InvalidTNetString, "no length found in #{data[offset, 10]}..." unless colon_index
-      offset + colon_index + 1
+      calculated_offset = offset + colon_index + 1
+      if !parent.nil?
+        parent_end = parent.term.offset + parent.term.length - 1
+        raise InvalidTNetString, "not inside scope (#{scope})" if calculated_offset > parent_end
+      end
+      calculated_offset
     end
 
     def raw_data=(new_raw_data)

spec/lazy_tnetstring/data_access_spec.rb
@@ -41,10 +41,15 @@ module LazyTNetstring
 
       context 'with parent' do
         let(:parent)      { mock('parent DataAccess') }
+        let(:term)        { mock('parent as Term') }
         let(:data)        { TNetstring.dump({}) }
         let(:data_access) { LazyTNetstring::DataAccess.new(data, 0, parent) }
 
         it "should add itself to the parent's children" do
+          parent.stub(:term).and_return(term)
+          term.stub(:offset).and_return(0)
+          term.stub(:length).and_return(data.length)
+
           parent.should_receive(:add_child).with(data_access)
         end
       end
@@ -86,6 +91,16 @@ module LazyTNetstring
 
         it 'should provide access to the inner hash' do
           subject['inner'].should == 'value'
+        end
+      end
+
+      context 'for nested hash with non-existing key' do
+        let(:data)              { TNetstring.dump({'outer' => { 'inner' => 'value'}, 'user' => {} }) }
+        let(:key)               { 'outer' }
+        let(:non_existing_key)  { 'non_existing_key' }
+
+        it 'should not find a value and terminate search' do
+          subject[non_existing_key].should be_nil
         end
       end
     end

commit 3bcf038ad29995bcf2f5cf2c6c60ae3a7e1bda3f

Fix bug when updating with a long value at the end
Bug introduced in 835da19a28402adb710dc7bf1fc97e519ed391bb

lib/lazy_tnetstring/data_access.rb
@@ -182,6 +182,7 @@ module LazyTNetstring
     end
 
     def term_following(term)
+      raise KeyNotFoundError if term.offset + term.length >= self.term.offset + self.term.length - 1
       term_at(term.offset + term.length, term.scope)
     end
 

lib/lazy_tnetstring/term.rb
@@ -59,12 +59,7 @@ module LazyTNetstring
     def calculated_value_offset
       colon_index = data[offset, 10].index(':')
       raise InvalidTNetString, "no length found in #{data[offset, 10]}..." unless colon_index
-      calculated_offset = offset + colon_index + 1
-      if !parent.nil?
-        parent_end = parent.term.offset + parent.term.length - 1
-        raise InvalidTNetString, "not inside scope (#{scope})" if calculated_offset > parent_end
-      end
-      calculated_offset
+      offset + colon_index + 1
     end
 
     def raw_data=(new_raw_data)

spec/lazy_tnetstring/data_access_spec.rb
@@ -297,6 +297,19 @@ module LazyTNetstring
           subject.data.should == new_data
         end
       end
+
+      context "when updating a key in a nested hash with a long value" do
+        let(:data)      { TNetstring.dump({ 'nested' => { key => '' } }) }
+        let(:key)       { 'key' }
+        let(:value)     { 'x' * 100 }
+        let(:new_data)  { TNetstring.dump({ 'nested' => { key => value } }) }
+
+        it "should update the key in the nested hash" do
+          subject['nested'] = {}
+          subject['nested'][key] = value
+          subject.data.should == new_data
+        end
+      end
     end
 
     describe "#remove" do